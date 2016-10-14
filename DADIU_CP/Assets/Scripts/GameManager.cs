a26c9101f9a385ad854a287fbcbec0b5aeff8b4b
DerpyMongoose <nicolajevers@hotmail.com>
2016-10-14 03:10:58 +0200

    Camera & Cutscene infrastructure

--- DADIU_CP/Assets/Scripts/GameManager.cs
+++ DADIU_CP/Assets/Scripts/GameManager.cs
@@ -5,6 +5,15 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    [HideInInspector]
+    public bool isCutscene = false;
+    [Header("Camera Controls")]
+    public float camX = 0;
+    public float camY = 7.5f;
+    public float camZ = -10f;
+    public float cutsceneDistance = 3;
+    public float cutsceneTime = 5;
+
     void Awake()
     {
         instance = this;
---
+++ DADIU_CP/Assets/Scripts/ZoomScene.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoomScene : MonoBehaviour
+{
+
+    public GameObject target;
+
+    private bool isActive;
+    private GameObject cam;
+    private CameraController cControl;
+
+    // Use this for initialization
+    void Start()
+    {
+        isActive = true;
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        cControl = cam.GetComponent<CameraController>();
+        if(cControl = null)
+        {
+            cam.AddComponent<CameraController>();
+        }
+    }
+
+    void Update()
+    {
+        if (GameManager.instance.isCutscene == true)
+        {
+            Debug.Log("isCutscene");
+            cControl.Cutscene(target);
+            GameManager.instance.isCutscene = false;
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        if (isActive == true)
+        {
+            Debug.Log("Trigger");
+            GameManager.instance.isCutscene = true;
+            isActive = false;
+        }
+    }
+}
