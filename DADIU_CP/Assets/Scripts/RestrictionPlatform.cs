cbb64011d23478d4620561c60b26b4b59a7e3779
Alexandros Panagiotis Giakalis <agiaka15@student.aau.dk>
2016-10-17 13:10:58 +0200

    Moving Platform

---
+++ DADIU_CP/Assets/Scripts/RestrictionPlatform.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestrictionPlatform : MonoBehaviour {
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.collider.tag == "Player")
+        {
+            agent.Stop();
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.collider.tag == "Player")
+        {
+            agent.Resume();
+        }
+    }
+}
