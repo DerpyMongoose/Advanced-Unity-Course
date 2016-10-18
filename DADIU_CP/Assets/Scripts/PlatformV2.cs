f8a1747a799a38ab06c98944f8bc7f42b1b1f582
Alexandros Panagiotis Giakalis <agiaka15@student.aau.dk>
2016-10-18 14:17:07 +0200

    New updated moving platform script

--- DADIU_CP/Assets/Scripts/PlatformV2.cs
+++ DADIU_CP/Assets/Scripts/PlatformV2.cs
@@ -1,104 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public class PlatformV2 : MonoBehaviour {
 
-public class Platform : MonoBehaviour
-{
     public GameObject patrol;
-    private Transform[] points;
+    public Transform[] points;
     public bool autoMove;
     private bool moveBack, left;
     private int destPoint = 0;
     private NavMeshAgent agent;
 
-
-    void Start()
-    {
+    void Start () {
         agent = GetComponentInParent<NavMeshAgent>();
         agent.updateRotation = false;
         points = new Transform[patrol.transform.childCount];
-        for (int i = 0; i < points.Length - 1; i++)
+        for (int i = 0; i < points.Length; i++)
         {
-            points[i] = patrol.transform.GetChild(i + 1);
-        }
-        points[points.Length - 1] = patrol.transform.GetChild(0);
-    }
-
-
-    void GotoNextPoint()
-    {
-        if (points.Length == 0)
-            return;
-
-        agent.destination = points[destPoint].position;      
-
-        if (autoMove)
-        {
-            if (!left)
-            {
-                destPoint = (destPoint + 1) % points.Length;
-                if (destPoint == points.Length - 1)
-                {
-                    left = true;
-                    destPoint = points.Length - 3;
-                }
-            }
-            else
-            {
-                destPoint = (int) nfmod((destPoint - 1), points.Length);
-                if(destPoint == points.Length - 1)
-                {
-                    left = false;
-                }
-            }
-        }
-        else
-        {
-            if(destPoint != points.Length - 2)
-            {
-              destPoint += 1;
-            }
+            points[i] = patrol.transform.GetChild(i);
         }
     }
-
-
-    void Update()
-    {
+	
+	void Update () {
+	
         if(moveBack)
         {
-            if (!autoMove)
+            if(!autoMove)
             {
                 if (agent.remainingDistance < 0.2f)
                 {
                     agent.destination = points[(destPoint + points.Length - 1) % points.Length].position;
                     destPoint--;
-                }
-                if (destPoint < 0)
-                {
-                    moveBack = false;
-                    destPoint = 0;
+                    if (destPoint == 0)
+                    {
+                        moveBack = false;
+                        destPoint = 0;
+                    }
                 }
             }
             else
             {
-
-                if (agent.remainingDistance < 0.2f)
+                if(agent.remainingDistance < 0.2f)
                 {
-                    if(!left)
+                    if (!left)
                     {
                         agent.destination = points[(int)nfmod((destPoint - 1), points.Length)].position;
                         destPoint--;
-                        if (destPoint < 0)
+                        if (destPoint == 0)
                         {
                             moveBack = false;
                             destPoint = 0;
                         }
                     }
-                    else // IT BREAKS AT SOME POINT.LOOK THE LOGIC.
+                    else
                     {
                         agent.destination = points[(int)nfmod(destPoint, points.Length)].position;
                         destPoint--;
-                        if (destPoint < -1)
+                        if (destPoint == -1)
                         {
                             moveBack = false;
                             left = false;
@@ -108,8 +65,46 @@ public class Platform : MonoBehaviour
                 }
             }
         }
-    }
 
+	}
+
+    void GotoNextPoint()
+    {
+        if (points.Length == 0)
+            return;
+
+        if(autoMove)
+        {
+            if (!left)
+            {
+                destPoint = (destPoint + 1) % points.Length;
+                if (destPoint == 0)
+                {
+                    left = true;
+                    destPoint = points.Length - 2;
+                }
+            }
+            else
+            {
+                destPoint = (int)nfmod((destPoint - 1), points.Length);
+                if (destPoint == points.Length - 1)
+                {
+                    left = false;
+                    destPoint = 1;
+                }
+            }
+        }
+        else
+        {
+            destPoint += 1;
+            if (destPoint == points.Length)
+            {
+                destPoint = points.Length - 1;
+            }
+        }
+
+        agent.destination = points[destPoint].position;
+    }
 
     void OnCollisionEnter(Collision col)
     {
@@ -118,7 +113,7 @@ public class Platform : MonoBehaviour
             CameraController.instance.player.transform.parent = transform;
             GotoNextPoint();
         }
-        
+
     }
 
     void OnCollisionStay(Collision col)
@@ -126,17 +121,21 @@ public class Platform : MonoBehaviour
         if (col.collider.tag == "Player")
         {
             if (agent.remainingDistance < 0.2f)
+            {
                 GotoNextPoint();
+            }
         }
     }
-    
+
     void OnCollisionExit(Collision col)
     {
         if (col.collider.tag == "Player")
         {
             CameraController.instance.player.transform.parent = null;
+            agent.Stop();
+            agent.ResetPath();
             moveBack = true;
-           
+
         }
     }
 
@@ -144,4 +143,5 @@ public class Platform : MonoBehaviour
     {
         return a - b * Mathf.Floor(a / b);
     }
+
 }
--- DADIU_CP/Assets/Scripts/RestrictionPlatform.cs
+++ DADIU_CP/Assets/Scripts/RestrictionPlatform.cs
@@ -5,6 +5,10 @@ public class RestrictionPlatform : MonoBehaviour {
 
     private NavMeshAgent agent;
 
+    //Ask about the issue of the rigidbodies when the platform is having remaining distance < 0.2f 
+    //then it pushes the player in order to finish its movement and as a result the player 
+    //is either pushed into the cubes or doing some weird movement to the side (due to the force the player receives).
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
